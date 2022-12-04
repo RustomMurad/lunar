9497900aaf0b10a568e5a9464c97298f274f2ba8
RustomMurad <RustomMurad@users.noreply.github.com>
2022-12-03 17:45:50 -0800

    Set Up

---
+++ src/main/java/qa/pragmatic/novalunium/NovaluniumApplication.java
@@ -0,0 +1,13 @@
+package qa.pragmatic.novalunium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaluniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaluniumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/qa/pragmatic/novalunium/NovaluniumApplicationTests.java
@@ -0,0 +1,13 @@
+package qa.pragmatic.novalunium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaluniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
